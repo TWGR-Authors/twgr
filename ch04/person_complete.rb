b5f18e8f3e0603eb5eb92e45e27a86ad1353a259
David A. Black <david@davidablack.net>
2025-09-28 12:21:09 -0400

    Cleaning up some duplicate files

--- ch04/person_complete.rb
+++ ch04/person_complete.rb
@@ -17,9 +17,8 @@ class Person
   end
 
   def self.method_missing(m, *args)
-    method = m.to_s
-    if method.start_with?("all_with_")
-      attr = method[9..-1]
+    if m.start_with?("all_with_")
+      attr = m[9..-1]
       if self.public_method_defined?(attr)
         PEOPLE.find_all do |person|
           person.send(attr).include?(args[0])
