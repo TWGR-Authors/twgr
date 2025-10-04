a8467898e9db146a39b9810d333d82be6c91d567
jleo3 <joseph.leo3@gmail.com>
2025-10-04 14:51:46 -0400

    Add another heredoc example

--- ch08/ruby_welcome_wagon.rb
+++ ch08/ruby_welcome_wagon.rb
@@ -1,10 +1,15 @@
 class RubyWelcomeWagon
-  def message
+  def hyphen_message
+    <<-EOM
+      Welcome to the world of Ruby!
+      In this method, the EOM doesn't have to be flush left!
+                 EOM
+  end
+
+  def squiggly_message
     <<~EOM
       Welcome to the world of Ruby!
       We're happy you're here!
     EOM
   end
 end
-
-RubyWelcomeWagon.new.message
